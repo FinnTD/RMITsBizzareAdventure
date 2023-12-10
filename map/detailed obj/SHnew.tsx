4a36f01ff0ee89632b1ca1122e4512c9084ed6ef
TuanChu1704 <118866105+TuanChu1704@users.noreply.github.com>
2023-12-10 21:26:28 +0700

    Add files via upload

---
+++ map/detailed obj/SHnew.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="SHnew" tilewidth="64" tileheight="64" tilecount="589" columns="31">
+ <image source="SHnew.png" trans="282738" width="2000" height="1250"/>
+</tileset>
---
+++ map/detailed obj/street.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="street" tilewidth="64" tileheight="64" tilecount="256" columns="16">
+ <image source="street.png" trans="282738" width="1024" height="1024"/>
+</tileset>
